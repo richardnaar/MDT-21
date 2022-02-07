b3c87275b93456d3fab0df37c52429c582b2907c
richardnaar <richardnaar@gmail.com>
2022-02-07 23:14:49 +0200

    added trig name find

--- generate triggers and validate/audio_trig_find.m
+++ generate triggers and validate/audio_trig_find.m
@@ -18,9 +18,9 @@ fs = newData1.fs;
 %% Double check trigger files
 
 fdat = abs(fft(data)).^2;
-% hz = 0:1/0.5:fs/2;
-% plot(hz(2:end), fdat(2:length(hz)) )
-plot(fdat)
+hz = 0:1/0.1:fs/2;
+plot(hz(2:end), fdat(2:length(hz)) )
+% plot(fdat)
 %% Find events in waveforms recorded during the experiment
 
 %% Import 2
@@ -54,6 +54,11 @@ plot(data(eindx(4):eindx(4)+fs/9))
 plot(data)
 hold on
 plot(event, 'r')
+%% find triggers
+trig = [110, 333, 554, 784, 1046]; 
+% trig(1) == start/end % trig(2) == fb/brush % trig(3) == dif/easy
+% trig(4:5) % out: 10 == less, 01 == same, 11 == greater
+
 %% plot event markers and waveforms on the same plot
 dur = fs/1;
 hz = 0:fs/dur:fs-1;
@@ -62,11 +67,53 @@ hz = 0:fs/dur:fs-1;
 for ii = 1:length(eindx)
     data1 = data(eindx(ii):eindx(ii)+dur);
 %     plot(data1)
-   fdat = abs(fft(data1)).^2;
-
+   fdat = abs(fft(data1)).^2/length(data1);
     plot(hz(2:length(hz)/2), fdat(2:length(hz)/2) )
 %      plot(hz(2:length(hz)/2), fdat(2:length(hz)/2) )
 %     plot(fdat)
+%% find triggers
+
+    raw_trig = find(abs(fdat(2:length(hz)/2))>10);
+    event = zeros(1, length(raw_trig));
+    for f_ind = 1:length(raw_trig)
+        idx = dsearchn(trig', raw_trig(f_ind));
+        if any(trig(idx) - raw_trig(f_ind) < 20) 
+            event(f_ind) = trig(idx);
+        end
+    end
+    event = unique(event);
+    event_i = zeros(1, length(trig));
+    for ei = 1:length(event)
+        idx = dsearchn(trig', event(ei));
+        event_i(ei) = 1;
+    end
+
+    if event_i(1)
+        fprintf('start\n')
+    else
+        fprintf('end\n')
+    end
+
+    if event_i(2)
+        fprintf('fb\n')
+    else
+        fprintf('brush\n')
+    end
+
+    if event_i(3)
+        fprintf('dif\n')
+    else
+        fprintf('easy\n')
+    end
+
+    if sum(event_i(4:5)) == 1
+        fprintf('greater\n')
+    elseif event_i(4) == 1
+        fprintf('less\n')
+    elseif event_i(5) == 1
+        fprintf('same\n')
+    end
+
     pause()
 end
 
